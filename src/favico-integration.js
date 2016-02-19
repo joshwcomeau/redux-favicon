c226c1a63dd21ca71b7d3488c6da574f1a2cefea
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-19 09:07:42 -0500

    Refactor middleware
    Separated the concerns by moving all favico-related stuff into a
    different file, and keeping all Redux action middleware stuff in
    the primary file.
    
    Also did some work to make the favico integration more extensible.
    There might be times where you want to set up different listeners;
    a blue notification when a new story is posted, a red one when you
    receive a direct message, etc.

---
+++ src/favico-integration.js
@@ -0,0 +1,74 @@
+import Favico from 'favico.js';
+
+const defaultFavicoOptions  = { animation: 'slide' };
+const favicoEnumValues      = [ 'increment', 'decrement', 'reset' ];
+
+
+export function create(options = defaultFavicoOptions) {
+  // Create a new Favico integration.
+  // Initially this was going to be a singleton object, but I realized there
+  // may be cases where you want several different types of notifications.
+  //
+  // Not using the constructor pattern, because it obfuscates JS' prototypal
+  // nature, and there's no need for inheritance here, so this is equivalent.
+  const favico = new Favico(options);
+
+  return {
+    // A secret access point, for the rare cases that the Favico instance
+    // needs to be interacted with directly.
+    __favico: favico,
+
+    currentVal: 0,
+
+    update(value, callback) {
+      if ( typeof value === 'number' ) {
+        // Don't allow non-integer values
+        if ( value % 1 !== 0 ) {
+          const errorMessage =`
+            Warning: Favico not affected.
+            You provided a floating-point value: ${value}.
+            You need to provide an integer, or a keyword value.
+            See <INSERT LINK> for more information.
+          `;
+          return callback(errorMessage);
+        }
+
+        this.currentVal = value;
+
+      } else if ( typeof value === 'string' ) {
+        switch ( value.toLowerCase() ) {
+          case 'increment': this.currentVal++; break;
+          case 'decrement': this.currentVal--; break;
+          case 'reset':     this.currentVal=0; break;
+          default:
+            const errorMessage = `
+              Warning: Favico not affected.
+              You provided a string value: ${value}.
+              The only strings we accept are: ${favicoEnumValues.join(', ')}.
+              See <INSERT LINK> for more information.
+            `);
+            return callback(errorMessage);
+        }
+
+      } else {
+        const errorMessage = `
+          Warning: Favico provided an illegal type.
+          You provided a a value of type: ${typeof value}.
+          We only accept integers or strings.
+          See <INSERT LINK> for more information.
+        `);
+        return callback(errorMessage);
+      }
+
+      // Don't allow negative numbers
+      this.currentVal = ( this.currentVal < 0 ) ? 0 : this.currentVal;
+
+      // Set the 'badge' to be our derived value.
+      // The favico.js library will show it if it's a positive number,
+      // or hide it if it isn't.
+      favico.badge(this.currentVal);
+
+      return callback();
+    }
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-// Add favicon tweaks in response to actions.
-import Favico from 'favico.js';
+import favicoIntegration from './favico-integration';
 
 
-const defaultFavicoOptions  = { animation: 'slide' };
-const favicoEnumValues      = [ 'increment', 'decrement', 'reset' ];
-
-export default function(favicoOptions = defaultFavicoOptions) {
-  const favico  = new Favico(favicoOptions);
-  let favicoVal = 0;
+export default function(favicoOptions) {
+  let favicon = favicoIntegration.create(favicoOptions)
 
   return store => next => action => {
     // Ignore actions that don't tweak the favico
@@ -15,47 +10,10 @@ export default function(favicoOptions = defaultFavicoOptions) {
       return next(action);
     }
 
-    // The 'favico' meta value can either be an integer,
-    // in which case we set it directly to that number.
-    // It also supports special strings 'increment'/'decrement'/'reset'.
-    const suppliedFavicoVal = action.meta.favico;
+    favicon.update(action.meta.favico, err => {
+      if (err) console.warn(err);
 
-    if ( typeof suppliedFavicoVal === 'number' ) {
-      // Don't allow non-integer values
-      if ( suppliedFavicoVal % 1 !== 0 ) {
-        console.warn(`
-          Warning: Favico not affected.
-          You provided a floating-point value: ${suppliedFavicoVal}.
-          You need to provide an integer, or a keyword value.
-          See <INSERT LINK> for more information.
-        `);
-        return next(action);
-      }
-
-      favicoVal = suppliedFavicoVal;
-    } else {
-      switch ( suppliedFavicoVal.toLowerCase() ) {
-        case 'increment': favicoVal++; break;
-        case 'decrement': favicoVal--; break;
-        case 'reset':     favicoVal=0; break;
-        default:
-          console.warn(`
-            Warning: Favico not affected.
-            You provided a string value: ${suppliedFavicoVal}.
-            The only strings we accept are 'increment', 'decrement' or 'reset'.
-            See <INSERT LINK> for more information.
-          `);
-          return next(action);
-      }
-    }
-
-    // Don't allow negative numbers
-    favicoVal = ( favicoVal < 0 ) ? 0 : favicoVal;
-
-    // Set the 'badge' to be our derived value.
-    // The favico.js library will show it if it's a positive number,
-    // or hide it if it isn't.
-    favico.badge(favicoVal);
-    return next(action);
+      return next(action);
+    })
   }
 }
