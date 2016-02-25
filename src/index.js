6aa3b2a1af359607320e53279f10ca87ae366df1
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-25 08:47:36 -0500

    Move integration back into src code
    It seemed silly to have such a small codebase split across two files.
    Once the code exceeds 100 lines, might be worth redoing it :)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import favicoIntegration from './favico-integration';
+import Favico from 'favico.js';
 
 
+// The middleware is kept as simple as possible. All favicon operations are
+// delegated to a function below.
 export default function(favicoOptions = {}) {
   // Detect if this middleware is being used without being 'preloaded',
   // by being passed a store instead of favicoOptions
@@ -11,16 +14,91 @@ export default function(favicoOptions = {}) {
   let favicon = favicoIntegration(favicoOptions)
 
   return store => next => action => {
-    console.log("Action:", action)
-    // Ignore actions that don't tweak the favico
-    if ( !action.meta || typeof action.meta.favico === 'undefined' ) {
+    // Ignore actions that don't tweak the favicon
+    if ( !action.meta || typeof action.meta.favicon === 'undefined' ) {
       return next(action);
     }
 
-    favicon.update(action.meta.favico, err => {
+    favicon.update(action.meta.favicon, err => {
       if (err) console.warn(err);
 
       return next(action);
     })
   }
+}
+
+const defaultFavicoOptions  = { animation: 'slide' };
+const favicoEnumValues      = [ 'increment', 'decrement', 'reset' ];
+
+// This integration communicates directly with Favico.js to set the badge.
+function favicoIntegration(options = defaultFavicoOptions) {
+  // Create a new Favico integration.
+  // Initially this was going to be a singleton object, but I realized there
+  // may be cases where you want several different types of notifications.
+  // The middleware does not yet support multiple instances, but it should
+  // be easy to add if there's demand :)
+  //
+  // Not using the constructor pattern, because it obfuscates JS' prototypal
+  // nature, and there's no need for inheritance here, so this is equivalent.
+  const favico = new Favico(options);
+
+  return {
+    // A secret access point, for the rare cases that the Favico instance
+    // needs to be interacted with directly.
+    __favico: favico,
+
+    currentVal: 0,
+
+    update(value, callback) {
+      console.log("Update called with value", value)
+      if ( typeof value === 'number' ) {
+        // Don't allow non-integer values
+        if ( value % 1 !== 0 ) {
+          const errorMessage =`
+            Warning: Favico not affected.
+            You provided a floating-point value: ${value}.
+            You need to provide an integer, or a keyword value.
+            See <INSERT LINK> for more information.
+          `;
+          return callback(errorMessage);
+        }
+
+        this.currentVal = value;
+
+      } else if ( typeof value === 'string' ) {
+        switch ( value.toLowerCase() ) {
+          case 'increment': this.currentVal++; break;
+          case 'decrement': this.currentVal--; break;
+          case 'reset':     this.currentVal=0; break;
+          default:
+            const errorMessage = `
+              Warning: Favico not affected.
+              You provided a string value: ${value}.
+              The only strings we accept are: ${favicoEnumValues.join(', ')}.
+              See <INSERT LINK> for more information.
+            `;
+            return callback(errorMessage);
+        }
+
+      } else {
+        const errorMessage = `
+          Warning: Favico provided an illegal type.
+          You provided a a value of type: ${typeof value}.
+          We only accept integers or strings.
+          See <INSERT LINK> for more information.
+        `;
+        return callback(errorMessage);
+      }
+
+      // Don't allow negative numbers
+      this.currentVal = ( this.currentVal < 0 ) ? 0 : this.currentVal;
+
+      // Set the 'badge' to be our derived value.
+      // The favico.js library will show it if it's a positive number,
+      // or hide it if it isn't.
+      favico.badge(this.currentVal);
+
+      return callback();
+    }
+  };
 }
